3b26ceaf7823f598545e769048cadaaea399c963
lxenard <laura.xenard@protonmail.com>
2025-10-24 16:20:08 +0200

    Add path to new test data

--- tmp_for_test.py
+++ tmp_for_test.py
@@ -1,8 +1,12 @@
 import epicure.epicuring as epicure
 
 
-main_path = "/media/lxenard/data/Code/EpiCure/trackMateMoiCa"
-raw_path = main_path + "/013_crop.tif"
+# main_path = "/media/lxenard/data/Code/EpiCure/trackMateMoiCa"
+# main_path = "/home/lxenard/snap/trackMateMoiCa"
+# raw_path = main_path + "/013_crop.tif"
+
+main_path = "/media/lxenard/data/Code/EpiCure/UnAutreExemplePourLaura_CaVaMarcher"
+raw_path = main_path + "/raw.tif"
 
 epic = epicure.EpiCure()
 epic.verbose = 3  # 0: minimal to 3: debug informations
